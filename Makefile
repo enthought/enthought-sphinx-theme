7abd85c9d0bfa4b1c9976c6d8fb4c91647a427c7
Robert Kern <rkern@enthought.com>
2014-10-07 10:47:28 +0100

    Make this an installable package.

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@
 
 all: css
 
-css: $(wildcard _theme/enthought/static/css/*.css)
+css: $(wildcard enthought_sphinx_theme/enthought/static/css/*.css)
 
-_theme/enthought/static/css/%.css: less/%.less
+enthought_sphinx_theme/enthought/static/css/%.css: less/%.less
 	lessc $^ > $@.new
 	mv -f $@.new $@
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='enthought_sphinx_theme',
+    version='0.5',
+    author='Enthought, Inc.',
+    author_email='info@enthought.com',
+    description='Sphinx theme for Enthought products',
+    packages=find_packages(),
+    include_package_data=True,
+)
