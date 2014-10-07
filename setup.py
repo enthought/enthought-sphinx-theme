7e953c9f622245884a09f70d7cb83f6c6f579ee3
Robert Kern <rkern@enthought.com>
2014-10-07 11:42:38 +0100

    DOC: Add licenses.

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+import os
+
 from setuptools import setup, find_packages
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+    long_description = f.read().strip()
 
 
 setup(
@@ -7,6 +13,17 @@ setup(
     author='Enthought, Inc.',
     author_email='info@enthought.com',
     description='Sphinx theme for Enthought products',
+    long_description=long_description,
+    url='https://github.com/enthought/enthought-sphinx-theme',
     packages=find_packages(),
     include_package_data=True,
+    license = "BSD",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Documentation",
+        "Topic :: Software Development :: Documentation",
+    ],
 )
