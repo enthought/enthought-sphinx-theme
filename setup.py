ccba5475f383e5963e7cb8b0dc64ff84b269bf9c
Corran Webster <cwebster@enthought.com>
2023-05-16 12:01:18 +0100

    Release 0.7.3
    Also includes a workflow to automatically publish on PyPI.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
 
 setup(
     name='enthought_sphinx_theme',
-    version='0.7.2',
+    version='0.7.3',
     author='Enthought, Inc.',
     author_email='info@enthought.com',
     description='Sphinx theme for Enthought products',
